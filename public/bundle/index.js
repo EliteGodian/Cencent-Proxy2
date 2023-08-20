f2f19031640301ad102d3fc772557c29d23fd9ea
EliteGodian <92820773+EliteGodian@users.noreply.github.com>
2023-08-20 14:43:58 -0500

    Update index.js

--- public/bundle/index.js
+++ public/bundle/index.js
@@ -19,13 +19,20 @@ const error = document.getElementById("uv-error");
  * @type {HTMLPreElement}
  */
 const errorCode = document.getElementById("uv-error-code");
+const urlParams = new URLSearchParams(window.location.search);
+
+
+
+
+  // Simulate pressing the Enter key
+  
 
 
 
 // Add a submit event listener to the form
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-
+  
   try {
     await registerSW();
   } catch (err) {
@@ -33,9 +40,10 @@ form.addEventListener("submit", async (event) => {
     errorCode.textContent = err.toString();
     throw err;
   }
-
+  const inputValue = urlParams.get('inputValue');
+  address.value = inputValue;
   // Get the filled out value from the 'uv-address' input
-  const inputAddressValue = address.value;
+  const inputAddressValue = inputValue;
 
   // Create the search URL using the filled out value and search engine
   const url = search(inputAddressValue, searchEngine.value);
@@ -43,11 +51,4 @@ form.addEventListener("submit", async (event) => {
   // Redirect to the URL
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('inputValue')) {
-  const inputValue = urlParams.get('inputValue');
-  address.value = inputValue;
 
-  // Simulate pressing the Enter key
-  form.submit();
-}
